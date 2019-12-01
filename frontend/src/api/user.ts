7eeb1ab02132567408ea6ecef7e4eef4e33f1cf9
Volodymyr Karpinskyi <bnmdude@gmail.com>
2019-12-02 00:07:03 +0100

    added friend list

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,12 +1,18 @@
 import axios from 'axios';
 import mainConfig from "../config";
-import {IResponse, UserDTO} from "../types";
+import {FriendDTO, IResponse, UserDTO} from "../types";
 
 interface IUserResponse extends IResponse {
     user: UserDTO
 }
 
+interface IFriendResponse extends IResponse {
+    friends: Array<FriendDTO>
+}
+
 export default {
     getAnotherUser: (id: string | number): Promise<IUserResponse> => axios.get(`${mainConfig.apiHost}/users/${id}`).then(res => res.data),
     addToFriends: (id: string | number): Promise<IUserResponse> => axios.post(`${mainConfig.apiHost}/friends/${id}`).then(res => res.data),
+    getFriends: (): Promise<IFriendResponse> => axios.get(`${mainConfig.apiHost}/friends`).then(res => res.data),
+    getFriendsRequest: (): Promise<IFriendResponse> => axios.get(`${mainConfig.apiHost}/friends/requests`).then(res => res.data),
 }
--- frontend/src/components/containers/User/UserInfoContainer.tsx
+++ frontend/src/components/containers/User/UserInfoContainer.tsx
@@ -1,28 +1,116 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {UserDTO} from "../../../types"
-import {Container, Grid, Segment, Header} from "semantic-ui-react";
+import {UserDTO, FriendDTO} from "../../../types"
+import {Container, Grid, Segment, Header, Card, Button, Image} from "semantic-ui-react";
+import userApi from "../../../api/user";
+import SegmentLoader from "../../base/SegmentLoader";
 
 interface IUserInfoContainerProps {
-    user: UserDTO
+    user: UserDTO,
 }
 
-class UserInfoContainer extends Component<IUserInfoContainerProps> {
+interface IUserInfoContainerState {
+    friends: Array<FriendDTO>,
+    friendsRequest: Array<FriendDTO>,
+    loading: boolean
+}
+
+class UserInfoContainer extends Component<IUserInfoContainerProps, IUserInfoContainerState> {
     static propTypes = {};
+
+    constructor(props: IUserInfoContainerProps) {
+        super(props);
+
+        this.state = {
+            friends: [],
+            friendsRequest: [],
+            loading: true
+        }
+    }
+
+    async componentDidMount(): Promise<void> {
+        const [friendsRequestResponse, friendsResponse] = await Promise.all([userApi.getFriendsRequest(), userApi.getFriends()]);
+        this.setState({
+            friends: friendsResponse.friends,
+            friendsRequest: friendsRequestResponse.friends,
+            loading: false
+        });
+    }
 
     render() {
         const {name, surname, email} = this.props.user;
-        return (
+        const {loading, friends, friendsRequest} = this.state;
 
-            <Grid celled='internally'>
-                <Grid.Row>
-                    <Grid.Column>
-                        <Header content={`Surname: ${surname}`}/>
-                        <Header content={`Name: ${name}`}/>
-                        <Header content={`Email: ${email}`}/>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+        return (
+            loading ?
+                <SegmentLoader/> :
+                <Grid celled='internally'>
+                    <Grid.Row width={12}>
+                        <Grid.Column width={12}>
+                            <Header content={`Surname: ${surname}`}/>
+                            <Header content={`Name: ${name}`}/>
+                            <Header content={`Email: ${email}`}/>
+                        </Grid.Column>
+                        <Grid.Column width={4}>
+                            {friendsRequest.length > 0 &&
+                            <Grid.Row>
+                                <Header textAlign={"center"} size={"huge"}> New friend requests </Header>
+                                <Card.Group>
+                                    {friendsRequest.map((friendRequest) =>
+                                        <Card fluid>
+                                            <Card.Content>
+                                                <Image floated='right'
+                                                       size='mini'
+                                                       src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+                                                />
+                                                <Card.Header>{friendRequest.Friend.name} {friendRequest.Friend.surname}</Card.Header>
+                                                <Card.Description>
+                                                    Steve wants to add you to the group <strong>best friends</strong>
+                                                </Card.Description>
+                                            </Card.Content>
+                                            <Card.Content extra>
+                                                <div className='ui two buttons'>
+                                                    <Button basic color='green'>
+                                                        Approve
+                                                    </Button>
+                                                    <Button basic color='red'>
+                                                        Decline
+                                                    </Button>
+                                                </div>
+                                            </Card.Content>
+                                        </Card>
+                                    )}
+                                </Card.Group>
+                            </Grid.Row>
+                            }
+                            <div>
+                                <h1> </h1>
+                            </div>
+                            {friends.length > 0 &&
+                            <Grid.Row>
+                                <Header textAlign={"center"} size={"huge"}> Friends </Header>
+                                <Card.Group>
+                                    {friends.map((friends) =>
+                                        <Card fluid>
+                                            <Card.Content>
+                                                <Image floated='right'
+                                                       size='mini'
+                                                       src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+                                                />
+                                                <Card.Header>{friends.Friend.name} {friends.Friend.surname}</Card.Header>
+                                                <Card.Description>
+                                                    Steve wants to add you to the group <strong>best friends</strong>
+                                                </Card.Description>
+                                            </Card.Content>
+                                        </Card>
+                                    )}
+                                </Card.Group>
+                            </Grid.Row>
+                            }
+
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
         );
     }
 }
@@ -31,8 +119,8 @@ UserInfoContainer.propTypes = {
     user: PropTypes.shape({
         name: PropTypes.string,
         surname: PropTypes.string,
-        email: PropTypes.string
-    })
+        email: PropTypes.string,
+    }),
 };
 
 
